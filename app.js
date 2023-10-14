2239516abc43570275cc24ec590ed45bc696c8f6
Anuj Jaglan <105397233+AJ4G4L4N@users.noreply.github.com>
2023-10-14 00:03:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Import required modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,42 +5,35 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs');
 
-// Import the index router module
 var indexRouter = require('./routes/index');
-
-// Create an Express application
-var app = express();
-
-// Configure view engine and partials
+// view engine setup
 hbs.registerPartials(path.join(__dirname, 'views/partials'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// Set up middleware
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the index router for the root path
 app.use('/', indexRouter);
 
-// Middleware for handling 404 errors
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Error handler middleware
+// error handler
 app.use(function(err, req, res, next) {
-  // Set locals for error handling
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Render the error page
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-// Export the app for use in other modules
 module.exports = app;
